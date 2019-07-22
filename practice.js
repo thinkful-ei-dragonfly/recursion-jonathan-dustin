21ec3038e252bdccc2790898402db0d376d51c27
Jonathan Moore <jmjonemoore@gmail.com>
2019-07-22 17:01:59 -0500

    Through

--- practice.js
+++ practice.js
@@ -124,12 +124,12 @@ function solveMaze(maze, x = 0, y = 0) {
   maze[y][x] = '*';
 
   // console.log(maze);
-  // console.log('move');
+  // console.log('move');  
 
   if (x + 1 >= maze[y].length || maze[y][x + 1] === '*') {
     if (maze[y + 1][x] !== '*' && y + 1 <= maze.length) {
       return 'D' + solveMaze(maze, x, y + 1);
-    } else if (y === 0 || maze[y-1][x] === '*') {
+    } else if (y === 0 || maze[y - 1][x] === '*') {
       return 'L' + solveMaze(maze, x - 1, y);
     }
     return 'U' + solveMaze(maze, x, y - 1);
@@ -138,7 +138,7 @@ function solveMaze(maze, x = 0, y = 0) {
   if (y + 1 >= maze.length || maze[y + 1][x] === '*') {
     if (x + 1 < maze[y].length && maze[y][x + 1] !== '*') {
       return 'R' + solveMaze(maze, x + 1, y);
-    } else if (x > 0 && maze[y][x-1] !== '*') {
+    } else if (x > 0 && maze[y][x - 1] !== '*') {
       return 'L' + solveMaze(maze, x - 1, y);
     }
     return 'U' + solveMaze(maze, x, y - 1);
@@ -166,6 +166,48 @@ let bigMaze2 = [
 //0 -132
 //1 -132
 //2 -
-console.log(solveMaze(mySmallMaze));
-console.log(solveMaze(bigMaze));
-console.log(solveMaze(bigMaze2));
+// console.log(solveMaze(mySmallMaze));
+// console.log(solveMaze(bigMaze));
+// console.log(solveMaze(bigMaze2));
+
+//9. Find All possible solutions to the maze
+
+function findAllMazeSolutions(maze, x = 0, y = 0, path = '') {
+
+  //Return an array of path strings
+
+  // Node 1 -> ['R']
+  // Node 2 -> ['RR']
+  // Node 3 -> ['RRD']
+  // Node 4 -> ['RRDD']
+  // Node 5 -> ['RRDDR', 'RRDDL']
+  // Node 6 -> ['RRDDRR', 'RRDDLL']
+  // Node 7 -> ['RRDDRRR', 'RRDDRRU', 'RRDDLLD']
+
+  if (maze[y][x] === 'e') {
+    console.log(path)
+    return
+  }
+
+  maze[y][x] = '*'
+
+  
+  if (y > 0 && maze[y - 1][x] && maze[y - 1][x] !== '*') {
+    findAllMazeSolutions(maze, x, y - 1, path + 'U')
+
+  }
+  if (x + 1 < maze[y].length && maze[y][x + 1] && maze[y][x + 1] !== '*') {
+    findAllMazeSolutions(maze, x + 1, y, path + 'R')
+
+  }
+  if (y + 1 < maze.length && maze[y + 1][x] && maze[y + 1][x] !== '*') {
+    findAllMazeSolutions(maze, x, y + 1, path + 'D')
+
+  }
+  if (x > 0 && maze[y][x - 1] && maze[y][x - 1] !== '*') {
+    findAllMazeSolutions(maze, x - 1, y, path + 'L')
+  }
+
+}
+
+console.log(findAllMazeSolutions(bigMaze))
